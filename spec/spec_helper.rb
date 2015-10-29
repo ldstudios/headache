261707102ce1a70b2d0a373cde4eaf219c25468f
Chris Bielinski <cbielinski@payoff.com>
2015-10-29 15:43:06 -0700

    code coverage -- the real way

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,13 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'factory_girl'
-require 'headache'
+require 'simplecov'
 require 'coveralls'
 
+::SimpleCov.add_filter 'spec'
 Coveralls.wear!
+
+require 'headache'
 
 Dir['./spec/support/**/*.rb'].each { |f| require f }
 Dir['./spec/factories/**/*.rb'].each { |f| require f }
