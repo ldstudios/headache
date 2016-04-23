7ae33fee1728ca709e305229ef26009d989d41f5
Scott LaBounty <slabounty@gmail.com>
2016-04-23 16:31:39 -0700

    Moved Document Tests to Correct Place

--- spec/headache/document_parser_spec.rb
+++ spec/headache/document_parser_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'active_support/core_ext/hash/indifferent_access'
 
 describe Headache::DocumentParser do
-  let(:document) { build :document }
   let(:ach_file) {
     File.open('./spec/fixtures/ach').read
     .gsub(Headache::DocumentParser::LINE_SEPARATOR, "\n")
@@ -17,19 +16,6 @@ describe Headache::DocumentParser do
     it 'can be dumped to a hash' do
       doc = described_class.new(ach_file).parse
       expect(doc.to_h.with_indifferent_access).to eq(ach_hash.with_indifferent_access)
-    end
-  end
-
-  describe "#generate" do
-    it 'generates the same document as it parses' do
-      doc = described_class.new(ach_file).parse
-      expect(doc.generate).to eq(ach_file)
-    end
-
-    it 'generates a document where each line is exactly 94 characters' do
-      document.generate.split(Headache::DocumentParser::LINE_SEPARATOR).each_with_index do |line, index|
-        expect(line.length).to eq(94)
-      end
     end
   end
 end
--- spec/headache/document_spec.rb
+++ spec/headache/document_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Headache::Document do
+  let(:document) { build :document }
+  let(:ach_file) {
+    File.open('./spec/fixtures/ach').read
+    .gsub(Headache::DocumentParser::LINE_SEPARATOR, "\n")
+    .gsub("\n", Headache::DocumentParser::LINE_SEPARATOR)
+  }
+
+  describe "#generate" do
+    it 'generates the same document as it parses' do
+      doc = Headache::DocumentParser.new(ach_file).parse
+      expect(doc.generate).to eq(ach_file)
+    end
+
+    it 'generates a document where each line is exactly 94 characters' do
+      document.generate.split(Headache::DocumentParser::LINE_SEPARATOR).each_with_index do |line, index|
+        expect(line.length).to eq(94)
+      end
+    end
+  end
 end
